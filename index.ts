27b8bc54b2f953fa112b29e42bcd9e5b28e162a9
xavier jouppe <xajouppe@gmail.com>
2024-10-13 20:05:08 +0200

    chore: use GH action instead of DENO deploy

--- index.ts
+++ index.ts
@@ -1,39 +1,38 @@
-import matrix from "npm:matrix-js-sdk";
-import { load } from "https://deno.land/std@0.224.0/dotenv/mod.ts";
-const env = await load();
+import matrix from 'matrix-js-sdk';
+import 'dotenv/config';
 
-const bot_user = "@surfbobot:matrix.org";
-const homeserver = "https://matrix.org";
+const bot_user = '@surfbobot:matrix.org';
+const homeserver = 'https://matrix.org';
 
 const client = matrix.createClient({
   baseUrl: homeserver,
-  accessToken: env.MATRIX_ACCESS_TOKEN,
+  accessToken: process.env.MATRIX_ACCESS_TOKEN,
   userId: bot_user,
 });
 
-const bret = "!OxZqETKZbkIBKswQPP:matrix.org";
+const bret = '!OxZqETKZbkIBKswQPP:matrix.org';
 await client.sendMessage(bret, {
-  msgtype: "m.notice",
+  msgtype: 'm.notice',
   body: "c'estmoi lebobot2",
 });
 // assumes bot_user has this format `@my-bot:matrix.org`
-const bot_name = bot_user.slice(1).split(":")[0];
+const bot_name = bot_user.slice(1).split(':')[0];
 // @ts-ignore NOTE: why does this not type check
-client.on("event", async (event: matrix.MatrixEvent) => {
+client.on('event', async (event: matrix.MatrixEvent) => {
   const roomId = event.getRoomId();
 
-  if (event.getType() === "m.room.message") {
+  if (event.getType() === 'm.room.message') {
     const message: string | undefined = event.event.content?.body;
     if (message?.startsWith(bot_name)) {
       const input = message
-        .replace(bot_name, "")
+        .replace(bot_name, '')
         // optional `:`
-        .replace(/^ *:/, "")
+        .replace(/^ *:/, '')
         .trim();
-      console.log("input:", input);
+      console.log('input:', input);
       if (roomId) {
         await client.sendMessage(roomId, {
-          msgtype: "m.notice",
+          msgtype: 'm.notice',
           body: evaljs(input),
         });
       }
@@ -42,7 +41,7 @@ client.on("event", async (event: matrix.MatrixEvent) => {
 });
 
 await client.startClient();
-console.log("Client Started");
+console.log('Client Started');
 
 // deno-lint-ignore no-explicit-any
 function evaljs(param: any) {
