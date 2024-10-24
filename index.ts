db841f5212cf1a9cf14fb3b21bbdd89f940e14bb
xavier jouppe <xajouppe@gmail.com>
2024-10-24 17:34:49 +0200

    style: format weekday

--- index.ts
+++ index.ts
@@ -4,21 +4,26 @@ import { send } from './sender';
 
 const playas = await fetcher();
 
+const getWeekDay = (d: Date) =>
+  new Intl.DateTimeFormat('fr-FR', { weekday: 'long' }).format(d);
+
 playas.forEach(async ({ html, playa, url }) => {
   const events = parser(html);
   const daysWithSurf = Object.keys(events);
 
   if (daysWithSurf.length > 0) {
     const body = `
-  🚨 swell alert - ${playa}
-  ${daysWithSurf
-    .map(
-      (day) =>
-        `${day} | ${events[day].map((e) => `${e.interval}: ${e.rating}⭐️`)}`
-    )
-    .join('\r\n')}
-  
-  👉 ${url}
+🚨 swell alert - ${playa}
+${daysWithSurf
+  .map(
+    (day) =>
+      `${getWeekDay(new Date(day))} | ${events[day].map(
+        (e) => `${e.interval}: ${e.rating}⭐️`
+      )}`
+  )
+  .join('\r\n')}
+
+👉 ${url}
       `;
 
     await send(body);
